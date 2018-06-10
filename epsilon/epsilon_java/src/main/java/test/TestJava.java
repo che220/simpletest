3c6f78698e5f3835081e223e9a8a16d36d964062
Your Name <you@example.com>
2018-06-10 08:20:12 -0500

    java shell

---
+++ epsilon/epsilon_java/src/main/java/test/TestJava.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestJava {
+	private static Logger logger = LoggerFactory.getLogger(TestJava.class);
+	
+	public static void main(String[] args) {
+		logger.info("abcdefeg");
+	}
+
+}
