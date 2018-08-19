5abbd66b8a67ff9317f9176c1d10308adf4e2385
hwang7 <hui_wang@intuit.com>
2018-08-19 18:22:23 -0500

    add OCR

---
+++ OCR/ocr-cv/TesseractTester.h
@@ -0,0 +1,17 @@
+/*
+ * TesseractTester.h
+ *
+ *  Created on: Aug 19, 2018
+ *      Author: hwang7
+ */
+
+#ifndef TESSERACTTESTER_H_
+#define TESSERACTTESTER_H_
+
+class TesseractTester {
+public:
+	TesseractTester();
+	virtual ~TesseractTester();
+};
+
+#endif /* TESSERACTTESTER_H_ */
